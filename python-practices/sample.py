0a08f85d56c64701b391459cc0f3f08722dba8d4
Mohammed Sinan C C <sinancc1018@gmail.com>
2026-04-23 16:43:05 +0530

    added sample.py

---
+++ python-practices/sample.py
@@ -0,0 +1,110 @@
+# a = 5
+# b = 2
+
+# print("sum =" ,a+b)
+# print("difference =" ,a-b)
+# print("product =" ,a*b)
+# print("division =" ,a/b)
+# print("modular division =" , a%b)
+# print("Exponent =", a ** b)
+# print(pow(a,b))
+
+# a = "60"
+# b = int(a)
+
+# print(b)
+
+# c = 80.9
+# d = int(c)
+
+# print(d)
+
+
+# name ="Sinan"
+# age = 20
+# score = 95.6
+
+# message = "My name is " + name + ", I am " + str(age) + " years old, and my score is " + str(score)
+# print(message)
+
+# using f-string
+
+# message1 = f"My name is {name}, I am {age} years old , and my score is {score}"
+# print(message1)
+
+# Using .format()
+
+# print("My name is {}, and I am {} years old, and my score is {}".format(name,age,score))
+
+# using name argument in .format()
+
+# student_name = "Ruby"
+# age = 50
+# message_template = "Hello, I am {s_name}. I am {a} years old"
+# m = message_template.format(s_name=student_name, a=age)
+# print(m)
+
+#FUNCTIONS
+
+# text = "Mohammed sinan"
+# print(text.capitalize())
+# print(text.lower())
+# print(text.upper())
+# print(text.swapcase())
+# print(len(text))
+# print(text.startswith("mo"))
+# print(text.endswith("an"))
+# print(text.count("n"))
+# print(text.find("i"))
+# print(text.split())
+# print("123".isnumeric())
+# print("abc".isalpha())
+# print("abc123".isalnum())
+
+#LIST
+
+# students = ["Sinan", "Ruby", "Muhammed"]
+
+# students[0] = "Sahal"
+# print(students)
+
+# students.append("Sijas")
+# print(students)
+
+# students.insert(2,"Misfer")
+# print(students)
+
+# students.remove("Ruby")
+# print(students)
+
+# students.pop(2)
+# print(students)
+
+# students.reverse()
+# print(students)
+
+# students.sort()
+# print(students)
+
+# students.sort(reverse=True)
+# print(students)
+
+#LIST SLICING
+
+# fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig','grape']
+
+# print(fruits)
+# print(fruits[:3])
+# print(fruits[2:6])
+# print(fruits[-3:])
+# print(fruits[::2])
+# print(fruits[::-1])
+
+#LIST SORTING
+# numbers = [5,2,9,1,7]
+# numbers.sort()
+# print(numbers)
+
+# numbers.sort(reverse=True)
+# print(numbers)
+
